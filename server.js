1bb90618411f5fb1bd15def5c54b7fe3fa77f5d9
Michael McBride <mcbride1987@gmail.com>
2025-09-24 12:05:23 -0700

    fix port number

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
 });
-server.listen(8000, '0.0.0.0', () => {
-    console.log('Server running at http://0.0.0.0:8000/');
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running at http://0.0.0.0:${PORT}/`);
 });
